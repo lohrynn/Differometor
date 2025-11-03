6c818df20460c1ec8092555c7ec3a41a4eeffeb0
Laurin <laurin.sefa@t-online.de>
2025-11-03 12:12:23 +0100

    First step of modularizing voyager optimization. Added ruff. Took notes.

--- examples/voyager_optimization.py
+++ examples/voyager_optimization.py
@@ -10,91 +10,157 @@ import optax
 import json
 
 
-### Calculate the target sensitivity ###
-#--------------------------------------#
+class VoyagerOptimization:
+    def __init__(self):
+        ### Calculate the target sensitivity ###
+        # --------------------------------------#
+        # use a predefined Voyager setup with one noise detector and two signal detectors
+        self._setup, component_property_pairs = voyager()
 
-# use a predefined Voyager setup with one noise detector and two signal detectors
-S, component_property_pairs = voyager()
+        # set the frequency range
+        self._frequencies = jnp.logspace(jnp.log10(20), jnp.log10(5000), 100)
 
-# set the frequency range
-frequencies = jnp.logspace(jnp.log10(20), jnp.log10(5000), 100)
+        # run the simulation with the frequency as the changing parameter
+        carrier, signal, noise, detector_ports, *_ = df.run(
+            self._setup, [("f", "frequency")], self._frequencies
+        )
 
-# run the simulation with the frequency as the changing parameter
-carrier, signal, noise, detector_ports, *_ = df.run(S, [("f", "frequency")], frequencies)
+        # calculate the signal power at the detector ports
+        powers = demodulate_signal_power(carrier, signal)
+        powers = powers[detector_ports]
 
-# calculate the signal power at the detector ports
-powers = demodulate_signal_power(carrier, signal)
-powers = powers[detector_ports]
+        # calculate the signal power from the two signal detectors for balanced homodyne detection
+        powers = powers[0] - powers[1]
 
-# calculate the signal power from the two signal detectors for balanced homodyne detection
-powers = powers[0] - powers[1]
+        # calculate the sensitivity
+        self._target_sensitivity = noise / jnp.abs(powers)
+        target_loss = jnp.sum(jnp.log10(self._target_sensitivity))
 
-# calculate the sensitivity
-target_sensitivity = noise / jnp.abs(powers)
-target_loss = jnp.sum(jnp.log10(target_sensitivity))
+        ### Start from random parameters and optimize the sensitivity ###
+        # ---------------------------------------------------------------#
+
+        # specify the ranges for the properties to be optimized
+        property_bounds = {
+            "reflectivity": [0, 1],
+            "tuning": [0, 90],
+            "db": [0.01, 20],
+            "angle": [-180, 180],
+            "power": [0.01, 200],
+            "mass": [0.01, 200],
+            "length": [1, 4000],
+            "phase": [-180, 180],
+        }
+
+        # select properties to be optimized
+        optimized_properties = [
+            "reflectivity",
+            "tuning",
+            "db",
+            "angle",
+            "power",
+            "mass",
+            "length",
+            "phase",
+        ]
+        self._optimization_pairs = []
+        for pair in component_property_pairs:
+            if pair[1] in optimized_properties:
+                self._optimization_pairs.append(pair)
+
+        # build the setup once and then reuse it during the optimization
+        simulation_arrays, detector_ports, *_ = df.run_build_step(
+            self._setup,
+            [("f", "frequency")],
+            self._frequencies,
+            self._optimization_pairs,
+        )
+
+        # calculate the bounds for the properties to be optimized
+        self._bounds = np.array(
+            [
+                [property_bounds[pair[1]][0], property_bounds[pair[1]][1]]
+                for pair in self._optimization_pairs
+            ]
+        ).T
+
+        # abstract for pure objective_function
+        bounds = self._bounds
+
+        def objective_function(optimized_parameters):
+            # map the parameters to between 0 and 1 and then to their respective bounds
+            optimized_parameters = sigmoid_bounding(optimized_parameters, bounds)
+            carrier, signal, noise = df.simulate_in_parallel(
+                optimized_parameters, *simulation_arrays[1:]
+            )
+            powers = demodulate_signal_power(carrier, signal)
+            powers = powers[detector_ports]
+            powers = powers[0] - powers[1]
+            sensitivity = noise / jnp.abs(powers)
+
+            # loss relative to target loss => loss < 0 is better than voyager setup
+            return jnp.sum(jnp.log10(sensitivity)) - target_loss
+
+        self.objective_function = objective_function
+
+    @property
+    def optimization_pairs(self):
+        return self._optimization_pairs
+
+    def output_to_files(self, best_params: None, losses: None, population_losses: None):
+        with open("voyager_optimization_parameters.json", "w") as f:
+            json.dump(best_params.tolist(), f, indent=4)
+
+        with open("voyager_optimization_losses.json", "w") as f:
+            json.dump(losses, f, indent=4)
+
+        plt.figure()
+        plt.plot(losses)
+        plt.xlabel("Iteration")
+        plt.ylabel("Loss")
+        plt.axhline(0, color="red", linestyle="--")
+        plt.grid()
+        plt.tight_layout()
+        plt.savefig("voyager_optimization_loss.png")
+
+        ### Calculate the sensitivity of the best found setup ###
+        # -------------------------------------------------------#
+
+        update_setup(best_params, self._optimization_pairs, self._bounds, self._setup)
+
+        carrier, signal, noise, detector_ports, *_ = df.run(
+            self._setup, [("f", "frequency")], self._frequencies
+        )
+        powers = demodulate_signal_power(carrier, signal)
+        powers = powers[detector_ports]
+        powers = powers[0] - powers[1]
+        sensitivity = noise / jnp.abs(powers)
+
+        plt.figure()
+        plt.plot(self._frequencies, sensitivity, label="Optimized Sensitivity")
+        plt.plot(
+            self._frequencies, self._target_sensitivity, label="Target Sensitivity"
+        )
+        plt.xscale("log")
+        plt.yscale("log")
+        plt.xlabel("Frequency (Hz)")
+        plt.ylabel("Sensitivity [/sqrt(Hz)]")
+        plt.legend()
+        plt.grid()
+        plt.tight_layout()
+        plt.savefig("voyager_optimization_sensitivity.png")
 
 
-### Start from random parameters and optimize the sensitivity ###
-#---------------------------------------------------------------#
+# Initialize setup
+vo = VoyagerOptimization()
+n_params = len(vo.optimization_pairs)
 
+initial_guess = jnp.array(np.random.uniform(-10, 10, n_params))
 
-# specify the ranges for the properties to be optimized
-property_bounds = {
-    "reflectivity": [0, 1],
-    "tuning": [0, 90],
-    "db": [0.01, 20],
-    "angle": [-180, 180],
-    "power": [0.01, 200],
-    "mass": [0.01, 200],
-    "length": [1, 4000],
-    "phase": [-180, 180],
-}
-
-# select properties to be optimized
-optimized_properties = ["reflectivity", "tuning", "db", "angle", "power", "mass", "length", "phase"]
-optimization_pairs = []
-for pair in component_property_pairs:
-    if pair[1] in optimized_properties:
-        optimization_pairs.append(pair)
-
-# build the setup once and then reuse it during the optimization
-simulation_arrays, detector_ports, *_ = df.run_build_step(
-    S,
-    [("f", "frequency")],
-    frequencies,
-    optimization_pairs,
-)
-
-# calculate the bounds for the properties to be optimized
-bounds = np.array([[
-    property_bounds[pair[1]][0], 
-    property_bounds[pair[1]][1]] for pair in optimization_pairs]).T
-
-# start from random parameters
-initial_guess = jnp.array(np.random.uniform(-10, 10, len(optimization_pairs)))
-
-
-def objective_function(optimized_parameters):
-    # map the parameters to between 0 and 1 and then to their respective bounds
-    optimized_parameters = sigmoid_bounding(optimized_parameters, bounds)
-    carrier, signal, noise = df.simulate_in_parallel(optimized_parameters, *simulation_arrays[1:])
-    powers = demodulate_signal_power(carrier, signal)
-    powers = powers[detector_ports]
-    powers = powers[0] - powers[1]
-    sensitivity = noise / jnp.abs(powers)
-
-    # loss relative to target loss => loss < 0 is better than voyager setup
-    return jnp.sum(jnp.log10(sensitivity)) - target_loss
-
-
-grad_fn = jax.jit(jax.value_and_grad(objective_function))
+grad_fn = jax.jit(jax.value_and_grad(vo.objective_function))
 # warmup the function to compile it
 _ = grad_fn(initial_guess)
 
-optimizer = optax.chain(
-    optax.clip_by_global_norm(1.0),
-    optax.adam(learning_rate=0.1)
-)
+optimizer = optax.chain(optax.clip_by_global_norm(1.0), optax.adam(learning_rate=0.1))
 optimizer_state = optimizer.init(initial_guess)
 
 best_loss, best_params = 1e10, initial_guess
@@ -120,42 +186,5 @@ for i in range(50000):
     if no_improve_count > 1000:
         break
 
-with open("voyager_optimization_parameters.json", "w") as f:
-    json.dump(best_params.tolist(), f, indent=4)
-
-with open("voyager_optimization_losses.json", "w") as f:
-    json.dump(losses, f, indent=4)
-
-plt.figure()
-plt.plot(losses)
-plt.xlabel("Iteration")
-plt.ylabel("Loss")
-plt.axhline(0, color="red", linestyle="--")
-plt.grid()
-plt.tight_layout()
-plt.savefig("voyager_optimization_loss.png")
-
-
-### Calculate the sensitivity of the best found setup ###
-#-------------------------------------------------------#
-
-update_setup(best_params, optimization_pairs, bounds, S)
-
-carrier, signal, noise, detector_ports, *_ = df.run(S, [("f", "frequency")], frequencies)
-powers = demodulate_signal_power(carrier, signal)
-powers = powers[detector_ports]
-powers = powers[0] - powers[1]
-sensitivity = noise / jnp.abs(powers)
-loss = jnp.sum(sensitivity)
-
-plt.figure()
-plt.plot(frequencies, sensitivity, label="Optimized Sensitivity")
-plt.plot(frequencies, target_sensitivity, label="Target Sensitivity")
-plt.xscale("log")
-plt.yscale("log")
-plt.xlabel("Frequency (Hz)")
-plt.ylabel("Sensitivity [/sqrt(Hz)]")
-plt.legend()
-plt.grid()
-plt.tight_layout()
-plt.savefig("voyager_optimization_sensitivity.png")
+# noqa
+vo.output_to_files(best_params, losses)
--- notes.md
+++ notes.md
@@ -2,6 +2,45 @@
 - Eine gute Initialisierung immer als richtlinie
 - Stabilität bei Initialisierung charakterisieren
 - Man kann noch eininges viel modularer machen! Aber erstmal verschiedene Algos ausprobieren von EvoX
+
+# Modularization changelog
+I am modularizing voyager_optimization to prepare for other algorithms and hyperparameter optimization. This should later be a subclass of a uifo optmization class which modularizes the more general optimization of a uifo setup. I think that starting with modularizing this more simple optimization is better.
+### Created `class VoyagerOptimization`
+- Moved much of the setup into `__init__()`
+- Properties are:
+  -  `optimization_pairs`
+- Methods of the class:
+  - `objective_function(optimized_parameters)` as a regular public attribute so it is pure and callable (wihtout self-reference and not as a property).
+    - This function gets defined in `__init__()` to maintain pureness (otherwise it would use mutable `self.` variables). TODO review if this is necessary. 
+  - `t2j(a)` for array conversion between `torch` and `jax`
+  - `j2t(a)`
+  - `output_to_files()` see below. TODO better name
+- Variables are (crossed ones were removed as a class variable):
+  - `self._setup`: Needed for `update_setup()` at file output.
+  - `self._frequencies`: Needed for `df.run()` at file output
+  - ~~`self._carrier`~~
+  - ~~`self._signal`~~
+  - ~~`self._noise`~~
+  - ~~`self._detector_ports`~~
+  - `self._target_sensitivity`
+  - ~~`self._target_loss`~~
+  - `self._optimization_pairs`: Needed for optimization algorithm
+  - ~~`self._simulation_arrays`~~
+  - ~~`self._detector_ports`~~
+  - `self._bounds`: Needed for `update_setup()` for file output.
+- File output is a new function `output_to_files()`
+  - Outputs  best paramters and losses (over time) into json
+  - Outputs Loss curve over time TODO option to plot over function calls.
+  - Outputs the final setup's sensitivity curve.
+  - Each argument can be left out which will just not plot/output the corresponding data.
+  - Maybe looking for some magic methods
+  - Arguments are:
+    - `best_params`: Params for the final setup that get printed
+    - `losses`: List of losses over time to plot
+    - `population_losses`: List of lists of losses representing each pop. Graph could get messy. TODO: find a good plotting representation for high population cases.
+  - Arguments TODO:
+    - Naming scheme and directory. I'm thinking of creating an output directory and adding some string argument to the file name. Also add the date & time to the file name for better sorting. Is it necessary to create an argument to add date & time (defaulting to True)??
+
 # Candidate Libraries
 ## PyGAD
 - not that great
