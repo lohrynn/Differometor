fab2018fb2e665ed70f12d6fad696da0995546e0
Laurin <laurin.sefa@student.uni-tuebingen.de>
2025-11-23 00:15:13 +0100

    changed Adam to not save optimized parameters between AdamGD.optimize() calls (+gitignore benchmarks)

--- optimization/algorithms/adam.py
+++ optimization/algorithms/adam.py
@@ -28,17 +28,18 @@ class AdamGD(OptimizationAlgorithm):
         self._problem = problem
         self.max_iterations = max_iterations
         self.patience = patience
-        self._best_params: Float[Array, "{self._problem.n_params}"] = jnp.array(
+        best_params: Float[Array, "{self._problem.n_params}"] = jnp.array(
             np.random.uniform(-10, 10, self._problem.n_params)
         )
-        self._losses = []
-        self._best_loss = 1e10
+        losses = []
+        best_loss = 1e10
 
         self._grad_fn = jax.jit(jax.value_and_grad(self._problem.objective_function))
 
     def optimize(
         self,
         save_to_file: bool = True,
+        init_params: Float[Array, "{self._problem.n_params}"] = None,
         return_best_params_history: bool = False,
         wall_time: float = None,
         learning_rate: float = 0.1,
@@ -54,16 +55,23 @@ class AdamGD(OptimizationAlgorithm):
             **adam_kwargs: Additional keyword arguments passed to optax.adam()
                           (Default: b1=0.9, b2=0.999, eps=1e-8, eps_root=0.0, nesterov=False)
         """
+        
+        # Initialize parameters
+        best_params: Float[Array, "{self._problem.n_params}"] = jnp.array(
+            np.random.uniform(-10, 10, self._problem.n_params)
+        ) if init_params is None else init_params
+        
         # warmup the function to compile it
-        _ = self._grad_fn(self._best_params)
+        _ = self._grad_fn(best_params)
 
         optimizer = optax.chain(
             optax.clip_by_global_norm(1.0), optax.adam(learning_rate, **adam_kwargs)
         )
-        optimizer_state = optimizer.init(self._best_params)
+        optimizer_state = optimizer.init(best_params)
 
-        params, losses = self._best_params, []
-        best_params_history = []  # Shape: (iterations, n_params)
+        params, losses = best_params, []
+        best_params_history = []  # later shape: (n_iterations, n_params)
+        best_loss = 1e10
 
         # Separate loops for wall-time constrained vs iteration/patience constrained
         if wall_time is not None:
@@ -76,12 +84,12 @@ class AdamGD(OptimizationAlgorithm):
                 if i % 100 == 0:
                     print(f"Iteration {i}: Loss = {loss}")
 
-                if loss < self._best_loss - 1e-4:
-                    self._best_loss, self._best_params = loss, params
+                if loss < best_loss - 1e-4:
+                    best_loss, best_params = loss, params
                     print(f"Iteration {i}: New best loss = {loss}")
 
                 if return_best_params_history:
-                    best_params_history.append(self._best_params)
+                    best_params_history.append(best_params)
 
                 updates, optimizer_state = optimizer.update(grads, optimizer_state, params)
                 params = optax.apply_updates(params, updates)
@@ -96,14 +104,14 @@ class AdamGD(OptimizationAlgorithm):
                 if i % 100 == 0:
                     print(f"Iteration {i}: Loss = {loss}")
 
-                if loss < self._best_loss - 1e-4:
-                    self._best_loss, self._best_params, no_improve_count = loss, params, 0
+                if loss < best_loss - 1e-4:
+                    best_loss, best_params, no_improve_count = loss, params, 0
                     print(f"Iteration {i}: New best loss = {loss}")
                 else:
                     no_improve_count += 1
 
                 if return_best_params_history:
-                    best_params_history.append(self._best_params)
+                    best_params_history.append(best_params)
 
                 updates, optimizer_state = optimizer.update(grads, optimizer_state, params)
                 params = optax.apply_updates(params, updates)
@@ -112,15 +120,15 @@ class AdamGD(OptimizationAlgorithm):
                 if no_improve_count > self.patience:
                     break
 
-        self._losses = jnp.array(losses)
-        self._best_params_history = jnp.array(best_params_history) if return_best_params_history else None
+        losses = jnp.array(losses)
+        best_params_history = jnp.array(best_params_history) if return_best_params_history else None
 
         if save_to_file:
             self._problem.output_to_files(
-                best_params=self._best_params,
-                losses=self._losses,
+                best_params=best_params,
+                losses=losses,
                 algorithm_str=self.algorithm_str,
                 hyper_param_str=f"lr{learning_rate}",
             )  # TODO maybe conditionally add more hyperparameters to string
 
-        return self._best_params, self._best_params_history, self._losses
+        return best_params, best_params_history, losses
